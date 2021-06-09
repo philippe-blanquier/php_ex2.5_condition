3b1af1f0d5e82238183f3ea4463319d1b378050d
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-09 13:08:17 +0200

    creation

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+        <title>PHP ex2.5 condition</title>
+    </head>
+    <body >
+        <?php
+        $gender="Home";
+        if( $gender != "Home")
+        {
+            echo ("C'est une développeuse !!!");
+        }
+        else
+        {
+            echo ("C'est un développeur !!!");
+        }
+       ?>
+    </body>
+</html>
